cb51cd98ef6d2fdc0b78011eb1f794cc70d92b3c
nikhil.mahajan <nikhil.mahajan@pepcus.com>
2020-12-06 22:06:11 +0530

    Material UI testing component.

--- demo/SplashPage.jsx
+++ demo/SplashPage.jsx
@@ -1,9 +1,14 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { Dashboard } from '@material-ui/icons';
 
 const SplashPage = () => (
-  <div />
+  <div>
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+    <Dashboard />
+  </div>
 );
 
 export default SplashPage;
